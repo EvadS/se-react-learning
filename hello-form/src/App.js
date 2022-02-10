35d8f1a7a5fd532420d325a37af1eeb6f19f9bb4
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2022-02-10 17:14:46 +0200

    Modal window

--- hello-form/src/App.js
+++ hello-form/src/App.js
@@ -6,6 +6,8 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
 
@@ -15,7 +17,8 @@ function App() {
         {id: 3, title: 'Python ', body: 'Description 3'}
     ]);
 
-    const [filter, setFilter] = useState({sort: '', query:''});
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
         console.log("get post")
@@ -31,6 +34,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false);
     };
 
     const removePost = (post) => {
@@ -38,20 +42,28 @@ function App() {
     }
 
     return (
-        <div>
-            <PostForm create={createPost}/>
-            <hr style={{margin: '15px 0'}}/>
-           <PostFilter
-               filter={filter}
-               setFilter={setFilter}
-           />
+        <div className="App">
 
-            {sortedAndSearchingPosts.length !== 0
-                ? <PostList remove={removePost} posts={sortedAndSearchingPosts} title="список постов "/>
-                : <h2 style={{textAlign: 'center'}}>
-                    Посты не найдены
-                </h2>
-            }
+            <MyButton
+                style={{marginTop:30}}
+                onClick={() => setModal(true)}
+            >
+                Создать
+            </MyButton>
+            <MyModal
+                visible={modal}
+                setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+
+            <PostList remove={removePost} posts={sortedAndSearchingPosts} title="список постов "/>
+
 
         </div>
     );
